63938c7efcde0785be6195a7e0624fa0bd9e5bdd
Eric Lemieux <ericlemieux2@gmail.com>
2013-11-09 15:39:24 -0500

    Adding scene graph class
    just creation, not in use yet

---
+++ SceneGraph.cpp
@@ -0,0 +1,79 @@
+#include "SceneGraph.h"
+
+Node::Node()
+{
+	translation = glm::mat4(1.0f);
+	rotX		= glm::mat4(1.0f);
+	rotY		= glm::mat4(1.0f);
+	rotZ		= glm::mat4(1.0f);
+}
+
+//Translate the node in 3D space
+void Node::translateNode(const glm::vec3 position)
+{
+	translation[3] = glm::vec4(position, 1.0f);
+}
+
+//Rotations around the x, y, and z axis
+void Node::rotateNodeX(const float degree)
+{
+	rotX[1] = glm::vec4(0, cos(degree * PI/180), sin(degree * PI/180), 0);
+	rotX[2] = glm::vec4(0, -sin(degree * PI/180), cos(degree * PI/180), 0);
+}
+void Node::rotateNodeY(const float degree)
+{
+	rotY[0] = glm::vec4(cos(degree * PI/180), 0, -sin(degree * PI/180), 0);
+	rotY[2] = glm::vec4(sin(degree * PI/180), 0, cos(degree * PI/180), 0);
+}
+void Node::rotateNodeZ(const float degree)
+{
+	rotZ[0] = glm::vec4(cos(degree * PI/180), sin(degree * PI/180), 0, 0);
+	rotZ[1] = glm::vec4(-sin(degree * PI/180), cos(degree * PI/180), 0, 0);
+}
+
+//Attach a node as a child object
+void Node::attachNode(Node *childNode)
+{
+	children.push_back(childNode);
+}
+//Detach a node child object
+void Node::detatchNode(Node *childNode)
+{
+	for(int i = 0; i < children.size(); ++i)
+	{
+		if(children[i] == childNode)
+		{
+			detatchNode(i);
+		}
+	}
+}
+void Node::detatchNode(const int index)
+{
+	children[index]			= 0;
+	children[index]->parent = 0;
+}
+//Detach all of the node children objects
+void Node::detatchAllNodes(void)
+{
+	children.clear();
+}
+
+//Update the node and its children
+void Node::update()
+{
+	localTransform = translation * rotX * rotY * rotZ;
+	worldTransform = parent->worldTransform * localTransform;
+
+	//Loop through all children and update
+	for(int i = 0; i < children.size(); ++i)
+	{
+		children[i]->update();
+	}
+}
+
+//return the world transformation matrix for the node
+//usefull for drawing what the node represents.
+glm::mat4 Node::getWorldTransform(void)
+{
+	return worldTransform;
+}
---
+++ SceneGraph.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vector>		//vector list
+#include <glm\glm.hpp>	//Vectors and Matricies
+#include <glm\ext.hpp>	//Quaternions
+
+#define PI 3.14159265359
+
+class Node
+{
+private:
+	std::vector<Node *> children;
+	Node *parent;
+
+	glm::mat4 localTransform;
+	glm::mat4 worldTransform;
+
+	glm::mat4 rotX;
+	glm::mat4 rotY;
+	glm::mat4 rotZ;
+	glm::mat4 translation;
+public:
+	Node();
+
+	//Translate the node in 3D space
+	void translateNode(const glm::vec3 position);
+
+	//Rotations around the x, y, and z axis
+	void rotateNodeX(const float degree);
+	void rotateNodeY(const float degree);
+	void rotateNodeZ(const float degree);
+
+	//Attach a node as a child object
+	void attachNode(Node *childNode);
+	//Detach a node child object
+	void detatchNode(Node *childNode);
+	void Node::detatchNode(const int index);
+	//Detach all of the node children objects
+	void detatchAllNodes(void);
+
+	//Update the node and its children
+	void update();
+
+	//return the world transformation matrix for the node
+	//usefull for drawing what the node represents.
+	glm::mat4 getWorldTransform(void);
+};
