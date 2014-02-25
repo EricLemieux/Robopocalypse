a2dccbebce262816c0a23de90dcccab1456e8ce2
Eric Lemieux <ericlemieux2@gmail.com>
2014-02-25 05:37:46 -0500

    Added texture class

---
+++ Texture.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+
+#include <GL\glew.h>
+
+#undef _UNICODE
+#include <IL\il.h>
+#include <IL\ilu.h>
+#include <IL\ilut.h>
+
+class Texture
+{
+public:
+	Texture();
+	Texture(char* filePath);
+	~Texture();
+
+	int Load(char* filePath);
+
+	inline GLuint GetHandle(void){ return handle; }
+
+private:
+	GLuint handle;
+};
