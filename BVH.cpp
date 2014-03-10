2c8e9d5f8f6896d5a6c7a264d4765b3ec10bab6f
Eric Lemieux <ericlemieux2@gmail.com>
2014-03-10 02:05:00 -0400

    Starting BVH loader

---
+++ BVH.cpp
@@ -0,0 +1,163 @@
+#include "BVH.h"
+
+
+BVH::BVH()
+{
+	this->filePath = new char;
+}
+
+BVH::BVH(char* filePath)
+{
+	LoadFile(filePath);
+}
+
+BVH::~BVH()
+{
+}
+
+void BVH::LoadFile(char* filePath)
+{
+	this->filePath = filePath;
+}
+
+int BVH::BuildSceneGraph()
+{
+	int currentIndex	= 0;
+	int targetIndex		= 0;
+
+	std::ifstream file;
+	file.open(filePath);
+
+	//Error if the file didnt open properly
+	if (!file.is_open())
+	{
+		std::cout << "Error opening the BVH file " << filePath << std::endl;
+		return BVH_H_LOAD_ERROR;
+	}
+	
+	char currentWord[256];
+
+	while (!file.eof())
+	{
+		file >> currentWord;
+
+		//This is the root node of the tree, everything else is attached to this 
+		if (!_stricmp(currentWord, "ROOT"))
+		{
+			//Generate a new node
+			Node* currentNode = new Node;
+
+			//Create the joint
+			Joint currentJoint;
+
+			currentIndex = targetIndex = 0;
+
+			//loop through the file before it describes the first joint node
+			while (_stricmp(currentWord, "JOINT"))
+			{
+				if (!_stricmp(currentWord, "OFFSET"))
+				{
+					glm::vec3 pos;
+
+					file >> currentWord;
+					pos.x = atof(currentWord);
+
+					file >> currentWord;
+					pos.y = atof(currentWord);
+
+					file >> currentWord;
+					pos.z = atof(currentWord);
+
+					currentNode->SetLocalPosition(pos);
+				}
+
+				else if (!_stricmp(currentWord, "CHANNELS"))
+				{
+					file >> currentWord;
+					currentJoint.numChanels = atoi(currentWord);
+
+					//Skip to the end of the line because we can assume the rest of the info based on the number of chanels
+					file.ignore(256, '\n');
+				}
+
+				file >> currentWord;
+			}
+
+			currentJoint.node = currentNode;
+
+			//Add the node to the vector
+			nodeTree.push_back(currentJoint);
+		}
+
+		
+
+		if (!_stricmp(currentWord, "JOINT"))
+		{
+			//Generate a new node
+			Node* currentNode = new Node;
+
+			//Create the joint
+			Joint currentJoint;
+
+			if (!_stricmp(currentWord, "{"))
+			{
+				currentIndex++;
+			}
+
+			if (!_stricmp(currentWord, "OFFSET"))
+			{
+				glm::vec3 pos;
+
+				file >> currentWord;
+				pos.x = atof(currentWord);
+
+				file >> currentWord;
+				pos.y = atof(currentWord);
+
+				file >> currentWord;
+				pos.z = atof(currentWord);
+
+				currentNode->SetLocalPosition(pos);
+			}
+
+			if (!_stricmp(currentWord, "CHANNELS"))
+			{
+				file >> currentWord;
+				currentJoint.numChanels = atoi(currentWord);
+
+				//Skip to the end of the line because we can assume the rest of the info based on the number of chanels
+				file.ignore(256, '\n');
+			}
+
+			if (!_stricmp(currentWord, "}"))
+			{
+				currentIndex--;
+			}
+
+			currentJoint.node = currentNode;
+
+			//Add the node to the vector
+			nodeTree.push_back(currentJoint);
+		}
+
+		if (!_stricmp(currentWord, "{"))
+		{
+			currentIndex++;
+		}
+
+		if (!_stricmp(currentWord, "}"))
+		{
+			currentIndex--;
+		}
+
+		//This part of the file is loading the rotations of joints
+		if (!_stricmp(currentWord, "MOTION"))
+		{
+			break;
+		}
+
+		file.ignore(256, '\n');
+	}
+
+	return BVH_H_LOAD_FINE;
+}
---
+++ BVH.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "SceneGraph.h"
+
+enum 
+{
+	BVH_H_LOAD_ERROR = 0,
+	BVH_H_LOAD_FINE,
+	BVH_F_LOAD_ERROR,
+	BVH_F_LOAD_FINE,
+};
+
+struct Joint
+{
+	Node* node;
+	int numChanels;
+};
+
+class BVH
+{
+public:
+	BVH();
+	BVH(char* filePath);
+	~BVH();
+
+	void LoadFile(char* filePath);
+
+	int BuildSceneGraph();
+
+private:
+	char* filePath;
+
+	std::vector<Joint> nodeTree;
+};
